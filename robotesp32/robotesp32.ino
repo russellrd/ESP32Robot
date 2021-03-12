9f5bf8ba5b4966e6cd59fa8b66de2cfe626c531c
russellrd <103starwars@gmail.com>
2021-03-11 20:44:55 -0500

    Basic Robot Code

---
+++ robotesp32/robotesp32.ino
@@ -0,0 +1,76 @@
+// Libraries
+#include <ESP32_Servo.h>
+
+// I/O
+#define TOUCH 15
+#define LED 13
+#define ECHO 5
+#define TRIG 2
+#define THRESH 10
+
+int pos = 0;
+long duration;
+double cm, inches;
+boolean start = false;
+
+// Drive servos
+Servo left, right;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  // Setup I/O
+  left.attach(32);
+  right.attach(33);
+  pinMode(LED, OUTPUT);
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+  digitalWrite(LED, LOW);
+}
+
+void loop() {
+  if(start) {
+    // Get distance from ultrasonic sensor
+    digitalWrite(LED, HIGH);
+    digitalWrite(TRIG, LOW);
+    delayMicroseconds(5);
+    digitalWrite(TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG, LOW);
+    
+    pinMode(ECHO, INPUT);
+    duration = pulseIn(ECHO, HIGH);
+    
+    cm = (duration/2.0) / 29.1;
+    inches = (duration/2.0) / 74.0;
+
+    // Drive forward until distance is less than 5 inches.
+    if(inches > 5) {
+      left.write(110);
+      right.write(65);
+    } else {
+      // Stop
+      left.write(90);
+      right.write(90);
+      delay(500);
+      // Move Back
+      left.write(65);
+      right.write(110);
+      delay(500);
+      /// Stop
+      left.write(90);
+      right.write(90);
+      delay(500);
+      // Turn Right
+      left.write(130);
+      right.write(130);
+      delay(200);
+    }
+  } else {
+     // Start moving when frame is touched
+     if(touchRead(TOUCH)<THRESH){
+      start = true;
+     }
+  }
+}
